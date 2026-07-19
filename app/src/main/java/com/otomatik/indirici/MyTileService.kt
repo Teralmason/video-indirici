34cf14e53c0db7c9cd51ebe9509ed61f8b85bede
Mehmet Emin Kartal <113544524+Teralmason@users.noreply.github.com>
2026-07-19 13:44:05 +0300

    Implement tile icon and state in MyTileService

--- app/src/main/java/com/otomatik/indirici/MyTileService.kt
+++ app/src/main/java/com/otomatik/indirici/MyTileService.kt
@@ -1,9 +1,19 @@
 package com.otomatik.indirici
 
+import android.graphics.drawable.Icon
+import android.service.quicksettings.Tile
 import android.service.quicksettings.TileService
 import android.widget.Toast
 
 class MyTileService : TileService() {
+
+    // Tile durum çubuğunda görünür hale geldiğinde (ikon, etiket ayarlanır)
+    override fun onStartListening() {
+        super.onStartListening()
+        qsTile.icon = Icon.createWithResource(this, R.drawable.ic_tile)
+        qsTile.state = Tile.STATE_ACTIVE
+        qsTile.updateTile()
+    }
 
     // Butona tıklandığında çalışacak olan fonksiyon
     override fun onClick() {
